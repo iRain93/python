e2ba2834c9f76b6bb03163ebc392b72eb755104e
iRain93 <1085594562@qq.com>
2020-05-17 12:47:03 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import sys
+print ("Python Version {}".format(str(sys.version).replace('\n', '')))
+print("显示方式：")  
+print("\033[0;37;40m\t方倍实验室\033[0m")  
+print("\033[1;37;40m\t方倍实验室\033[0m") 
+print("\033[22;37;40m\t方倍实验室\033[0m")  
+print("\033[4;37;40m\t方倍实验室\033[0m") 
+print("\033[24;37;40m\t方倍实验室\033[0m")  
+print("\033[5;37;40m\t方倍实验室\033[0m") 
+print("\033[25;37;40m\t方倍实验室\033[0m")  
+print("\033[7;37;40m\t方倍实验室\033[0m") 
+print("\033[27;37;40m\t方倍实验室\033[0m")
+input("Press Enter key to exit")
